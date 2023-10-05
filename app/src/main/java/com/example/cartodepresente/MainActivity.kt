745b418c983a9618ff17790fe382f5f10949789a
joaozeta <melodelimajoaovictor@gmail.com>
2023-10-05 16:19:12 -0300

    mensagem

--- app/src/main/java/com/example/cartodepresente/MainActivity.kt
+++ app/src/main/java/com/example/cartodepresente/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.cartodepresente
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cartodepresente.ui.theme.CartãoDePresenteTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CartãoDePresenteTheme {
+                telaCartaoPresente()
             }
         }
     }
@@ -24,10 +40,54 @@ class MainActivity : ComponentActivity() {
 @Preview (showSystemUi = true)
 @Composable
 fun telaCartaoPresente(){
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.imagem7),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+            alpha = 0.9F
+        )
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+
+            Text(
+                text = "Feliz Aniversário Mamãe!",
+                fontSize = 54.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(top = 250.dp),
+                lineHeight = 100.sp
+            )
+            Dedicatoria(text = "de: Manu")
+            Dedicatoria(text = "para: Dani Milfont")
+        }
+    }
+}
+@Composable
+fun Dedicatoria(text: String){
     Text(
-        text = "Feliz Aniversário Mamãe!"
+        text = text,
+        fontSize = 20.sp,
+        color = Color.Black,
+        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.Monospace,
+        textAlign = TextAlign.End,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 25.dp)
+
     )
-    Text(
-        text = "para: Daniela Milfont" )
+
+
 }
 
